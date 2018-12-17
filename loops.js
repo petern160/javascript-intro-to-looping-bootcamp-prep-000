cd8ca1f59e5caa88f36f338e42c61d1df1c490f7
Peter Nguyen <peternguyen160@gmail.com>
2018-12-17 05:46:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,9 +15,16 @@ function whileLoop(j){
   return "done"
 }
 
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 function doWhileLoop(array){
 do {
   array.pop()
 } while (array.length>0 && maybeTrue())
 return array
 }
+
+do_while_arr = ['a','b','c'];
+console.log(doWhileLoop(do_while_arr))
