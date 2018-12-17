fea7f72fa7052eedd0784a9775652c5e7e52a23b
Peter Nguyen <peternguyen160@gmail.com>
2018-12-17 05:38:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-function forLoop(array) {
+function forLoop(a) {
    for ( let i = 0; i < 25; i++) {
      if (i === 1) {
-       array.push(`I am 1 strange loop.`)
+       console.log(`I am 1 strange loop.`)
      } else {
-       array.push(`I am ${i} strange loops.`)
+       console.log(`I am ${i} strange loops.`)
      }
   }
-  return array;
+  return a;
 }
