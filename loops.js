c5e6690fff654f779f24fbafbd158378ff5f78b0
Peter Nguyen <peternguyen160@gmail.com>
2018-12-17 05:42:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-function forLoop(j) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+function forLoop(array){
+  for(var i = 0; i <25; i++){
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
   }
-  return j;
+}
+return array
 }
